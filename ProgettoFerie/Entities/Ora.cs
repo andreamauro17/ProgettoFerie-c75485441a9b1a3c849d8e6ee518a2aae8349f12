8582788bcfcad41bd5256c820fa8605c978aea4f
andreamauro17 <andreamauro01@gmail.com>
2020-04-29 09:45:32 +0200

    Aggiungere i file di progetto.

---
+++ ProgettoFerie/Entities/Ora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ProgettoFerie.Entities
+{
+    public class Ora
+    {
+        public int Id { get; set; }
+
+        public DateTime Data { get; set; }
+        public int NumeroOra { get; set; }
+
+        public string IdInsegnanteDisposizione { get; set; }
+        public virtual ApplicationUser InsegnanteDisposizione { get; set; }
+        public virtual ApplicationUser InsegnanteUtilizzo { get; set; }
+        public virtual Ricorsione Ricorsione { get; set; }
+    }
+}
